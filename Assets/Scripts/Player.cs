9f72fe6cdb77598512cd68bea5893a8b2799462b
Flavia Nunes <flavianunesdev@gmail.com>
2021-02-28 15:23:04 -0300

    personagem corre

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,31 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private CharacterController controller;
+    public Rigidbody rb;
+    public float speed;
+    public float jumpHeight;
+    private float jumpSpeed = 50;
+    public float gravity;
+
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        rb = GetComponent <Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Makes character run        
+        transform.Translate(Vector3.forward * (speed) * Time.deltaTime);
+
+        // Make character jump - TODO
+        // if (Input.GetKeyDown(KeyCode.Space)) {
+        //     rb.AddForce(Vector3.up * jumpSpeed);
+        // }
+
     }
 }
+
+
